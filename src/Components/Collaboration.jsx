a1f3cd85781a1126cb5af84a5b588d8dc863d82b
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-09-13 18:00:27 +0530

    Update Collaboration.jsx

--- src/Components/Collaboration.jsx
+++ src/Components/Collaboration.jsx
@@ -4,6 +4,10 @@ import Section from "./Section";
 import Button from "./Button";
 import {LeftCurve, RightCurve} from './design/Collaboration'
 
+
+
+
+
 const Collaboration = () => {
   return (
     <Section crosses>
