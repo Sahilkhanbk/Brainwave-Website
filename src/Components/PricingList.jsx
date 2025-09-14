e29ca3d56e7cdb482dc3a9f4636a9542750d4d05
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-09-14 23:08:25 +0530

    Update PricingList.jsx

--- src/Components/PricingList.jsx
+++ src/Components/PricingList.jsx
@@ -2,6 +2,13 @@ import Button from "./Button";
 import { pricing } from "../constants/Index";
 import { check } from "../assets";
 
+
+
+
+
+
+
+
 const PricingList = () => {
   return (
     <div className="flex gap-[1rem] max-lg:flex-wrap">
